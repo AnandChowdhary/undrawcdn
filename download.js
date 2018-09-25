b8b1a8d7ac209a235c09886419da339a0a4466fd
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-25 09:26:49 +0200

    Download files

--- download.js
+++ download.js
@@ -1,7 +1,15 @@
 // node -e 'require("./download")()'
 
+const https = require("https");
+const fs = require("fs");
+const slugify = require("slugify");
 const icons = require("./icons.json");
 
 module.exports = () => {
-	console.log("Hello!");
+	for (icon in icons) {
+		const slug = slugify(icon, { lower: true });
+		const url = icons[icon];
+		const file = fs.createWriteStream(`${slug}.svg`);
+		https.get(url, response => response.pipe(file));
+	}
 };
